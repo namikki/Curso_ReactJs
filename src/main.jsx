6b120d634b3dcaff9637ca0ce9d7ace58e8185bb
Bianca da Silva Rodrigues <iambiancarodrigues@gmail.com>
2025-04-15 09:43:06 -0300

    [feat] atualizando a página main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './pages/App';
 
+// Criação do cliente do React Query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5, // 5 minutos
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
