79f7d32714546391fa469e7f02f688aaf3133a3c
Bianca da Silva Rodrigues <iambiancarodrigues@gmail.com>
2025-04-15 09:51:26 -0300

    [feat] atualizando Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,30 @@
-import { NavLink } from 'react-router-dom'; // Importa NavLink
+import { NavLink } from 'react-router-dom';
 
-// Recebe cartCount como prop
 const Header = ({ cartCount = 0 }) => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container">
-          {/* Usa NavLink também para o brand, se desejar navegação SPA */}
-          <NavLink className="navbar-brand" to="/">React Router App</NavLink>
+          <NavLink className="navbar-brand" to="/">React Shop</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuPrincipal">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="menuPrincipal">
             <div className="navbar-nav">
-              {/* Usa NavLink para os links de navegação */}
               <NavLink className="nav-link" to="/" end>
                 Home
               </NavLink>
               <NavLink className="nav-link" to="/produtos">
                 Produtos
               </NavLink>
-              {/* Mantendo como 'a' por enquanto, pois não há rotas definidas */}
+              <NavLink className="nav-link" to="/produtos/novo">
+                Novo Produto
+              </NavLink>
               <a className="nav-link" href="/sobre">Quem Somos</a>
               <a className="nav-link" href="/contato">Contato</a>
             </div>
-            {/* Exibe a contagem do carrinho no final da navbar */}
-            <span className="navbar-text ms-auto"> {/* ms-auto alinha à direita */}
+            <span className="navbar-text ms-auto">
               Carrinho:
-              {/* Atualizando badge para primary para melhor contraste */}
               <span className="badge bg-primary rounded-pill ms-2">{cartCount}</span>
             </span>
           </div>
